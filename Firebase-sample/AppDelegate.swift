560875065684f87ffe54b4bd95cd8d07a72ed508
Elaidzha1940 <eli080293@gmail.com>
2024-04-09 00:53:44 +0300

    Update Pods

--- Firebase-sample/AppDelegate.swift
+++ Firebase-sample/AppDelegate.swift
@@ -1,20 +1,18 @@
+//  /*
 //
-//  AppDelegate.swift
-//  Firebase-sample
+//  Project: Firebase-sample
+//  File: AppDelegate.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 07.04.2024
 //
-//  Created by Elaidzha Shchukin on 07.04.2024.
-//
+//  */
 
 import UIKit
-import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         return true
     }
 
@@ -31,7 +29,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
